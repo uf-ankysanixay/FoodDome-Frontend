84ea02e251a8782bab190b56066a229dd4d80abe
ankysanixay <anky@sanixay.com>
2024-11-20 15:05:30 -0500

    Prod updates

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -8,22 +8,18 @@ interface Config {
 }
 
 const config: Config = {
-  apiUrl:
-      process.env.VITE_API_URL ||
-      import.meta.env.VITE_API_URL ||
-      'http://localhost:5000/api',
-  webUrl:
-      process.env.VITE_WEB_URL ||
-      import.meta.env.VITE_WEB_URL ||
-      'http://localhost:4000',
-  environment:
-      process.env.VITE_ENV ||
-      import.meta.env.VITE_ENV ||
-      'development',
+  apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  webUrl: import.meta.env.VITE_WEB_URL || 'http://localhost:4000',
+  environment: import.meta.env.VITE_ENV || 'development',
   isProduction: false, // Default value
 };
 
 // Set isProduction based on the environment
 config.isProduction = config.environment === 'production';
+
+// Debug log during development
+if (!config.isProduction) {
+  console.log('Config:', config);
+}
 
 export default config;
